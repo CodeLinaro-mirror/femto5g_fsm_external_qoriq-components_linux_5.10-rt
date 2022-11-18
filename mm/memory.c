2dd5ff04e05cbf3720edfee72fed30b11e06857f
Suren Baghdasaryan <surenb@google.com>
2022-11-18 15:36:13 -0800

    ANDROID: mm: prevent reads of unstable pmd during speculation
    Checks of pmd during speculative page fault handling are racy because
    pmd is unprotected and might be modified or cleared. This might cause
    use-after-free reads from speculative path, therefore prevent such
    checks. At the beginning of speculation pmd is checked to be valid and
    if it's changed before page fault is handled, the change will be detected
    and page fault will be retried under mmap_lock protection.
    
    Bug: 257443051
    Change-Id: I0cbd3b0b44e8296cf0d6cb298fae48c696580068
    CRs-Fixed: 3393872
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Git-commit: 2bb39b912175c3c087978ae5547e277a8422c601
    Git-repo: https://android.googlesource.com/kernel/common/
    [quic_c_spathi@quicinc.com: resolve merge conflicts]
    Signed-off-by: Srinivasarao Pathipati <quic_c_spathi@quicinc.com>
    Signed-off-by: Sivareddy Surasani <quic_ssurasan@quicinc.com>

--- mm/memory.c
+++ mm/memory.c
@@ -3606,6 +3606,10 @@ static vm_fault_t __do_fault(struct vm_fault *vmf)
 	struct vm_area_struct *vma = vmf->vma;
 	vm_fault_t ret;
 
+	/* Do not check unstable pmd, if it's changed will retry later */
+	if (vmf->flags & FAULT_FLAG_SPECULATIVE)
+		goto skip_pmd_checks;
+
 	/*
 	 * Preallocate pte before we take page_lock because this might lead to
 	 * deadlocks for memcg reclaim which waits for pages under writeback:
@@ -3628,6 +3632,7 @@ static vm_fault_t __do_fault(struct vm_fault *vmf)
 		smp_wmb(); /* See comment in __pte_alloc() */
 	}
 
+skip_pmd_checks:
 	ret = vma->vm_ops->fault(vmf);
 	if (unlikely(ret & (VM_FAULT_ERROR | VM_FAULT_NOPAGE | VM_FAULT_RETRY |
 			    VM_FAULT_DONE_COW)))
@@ -3976,7 +3981,8 @@ static vm_fault_t do_fault_around(struct vm_fault *vmf)
 	end_pgoff = min3(end_pgoff, vma_pages(vmf->vma) + vmf->vma->vm_pgoff - 1,
 			start_pgoff + nr_pages - 1);
 
-	if (pmd_none(*vmf->pmd)) {
+	if (!(vmf->flags & FAULT_FLAG_SPECULATIVE) &&
+	    pmd_none(*vmf->pmd)) {
 		vmf->prealloc_pte = pte_alloc_one(vmf->vma->vm_mm);
 		if (!vmf->prealloc_pte)
 			goto out;
@@ -4350,6 +4356,10 @@ static vm_fault_t handle_pte_fault(struct vm_fault *vmf)
 {
 	pte_t entry;
 
+	/* Do not check unstable pmd, if it's changed will retry later */
+	if (vmf->flags & FAULT_FLAG_SPECULATIVE)
+		goto skip_pmd_checks;
+
 	if (unlikely(pmd_none(*vmf->pmd))) {
 		/*
 		 * Leave __pte_alloc() until later: because vm_ops->fault may
@@ -4386,6 +4396,7 @@ static vm_fault_t handle_pte_fault(struct vm_fault *vmf)
 		}
 	}
 
+skip_pmd_checks:
 	if (!vmf->pte) {
 		if (vma_is_anonymous(vmf->vma))
 			return do_anonymous_page(vmf);
